fdd29dadffa73ad81b256e1233c2e4a1b95ab951
Isaac Shin <113717874+EyeSoLotion@users.noreply.github.com>
2023-09-13 18:45:31 -0500

    Finished up all numeric inputs as well as answerButton code
    9/13/23

--- app/src/main/java/com/example/calculatorappshin/MainActivity.java
+++ app/src/main/java/com/example/calculatorappshin/MainActivity.java
@@ -11,13 +11,12 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    // Global variables
     boolean nextTextView;
     EditText num1;
     EditText num2;
     TextView operator;
     TextView answer;
-    String buttonString;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Operator methods
     public void subtract(View view) {
         Button subtract = findViewById(R.id.subButton);
         String buttonSubString = subtract.getText().toString();
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         operator.setText(buttonSubString);
         nextTextView = true;
     }
-
     public void addition(View view) {
         Button addition = findViewById(R.id.addButton);
         String buttonAddString = addition.getText().toString();
@@ -40,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         operator.setText(buttonAddString);
         nextTextView = true;
     }
-
     public void multiply(View view) {
         Button multiply = findViewById(R.id.multButton);
         String buttonMultString = multiply.getText().toString();
@@ -48,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         operator.setText(buttonMultString);
         nextTextView = true;
     }
-
     public void divide(View view) {
         Button divide = findViewById(R.id.divButton);
         String buttonDivString = divide.getText().toString();
@@ -57,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         nextTextView = true;
     }
 
+    // Number input methods
     public void enterOne(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
@@ -93,81 +91,92 @@ public class MainActivity extends AppCompatActivity {
     public void enterFour(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonFour = findViewById(R.id.buttonFour);
+        String buttonFourString = buttonFour.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonFourString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonFourString);
         }
     }
     public void enterFive(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonFive = findViewById(R.id.buttonFive);
+        String buttonFiveString = buttonFive.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonFiveString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonFiveString);
         }
     }
     public void enterSix(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonSix = findViewById(R.id.buttonSix);
+        String buttonSixString = buttonSix.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonSixString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonSixString);
         }
     }
     public void enterSeven(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonSeven = findViewById(R.id.buttonSeven);
+        String buttonSevenString = buttonSeven.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonSevenString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonSevenString);
         }
     }
     public void enterEight(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonEight = findViewById(R.id.buttonEight);
+        String buttonEightString = buttonEight.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonEightString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonEightString);
         }
     }
     public void enterNine(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonNine = findViewById(R.id.buttonNine);
+        String buttonNineString = buttonNine.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonNineString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonNineString);
         }
     }
     public void enterZero(View view){
         num1 = findViewById(R.id.inputText1);
         num2 = findViewById(R.id.inputText2);
-        Button buttonOne = findViewById(R.id.buttonOne);
-        String buttonOneString = buttonOne.getText().toString();
+        Button buttonZero = findViewById(R.id.buttonZero);
+        String buttonZeroString = buttonZero.getText().toString();
         if(nextTextView == false){
-            num1.append(buttonOneString);
+            num1.append(buttonZeroString);
         }else{
-            num2.append(buttonOneString);
+            num2.append(buttonZeroString);
         }
     }
 
+    public void addPoint(View view){
+        num1 = findViewById(R.id.inputText1);
+        num2 = findViewById(R.id.inputText2);
+        if(nextTextView == false){
+            num1.append(".");
+        }else{
+            num2.append("");
+        }
+    }
+
+    // Show answer method
     public void answerEnter(View view){
         String finalAnswer = "";
         num1 = findViewById(R.id.inputText1);
@@ -180,9 +189,19 @@ public class MainActivity extends AppCompatActivity {
 
         if(operatorString.equalsIgnoreCase("-")){
             finalAnswer = String.valueOf((inputText1 - inputText2));
-            num1.setText("");
-            num2.setText("");
-            operator.setText("");
+            num1.setText("");num2.setText("");operator.setText("");
+            nextTextView = false;
+        }else if(operatorString.equalsIgnoreCase("+")){
+            finalAnswer = String.valueOf((inputText1 + inputText2));
+            num1.setText("");num2.setText("");operator.setText("");
+            nextTextView = false;
+        }else if(operatorString.equalsIgnoreCase("x")){
+            finalAnswer = String.valueOf((inputText1 * inputText2));
+            num1.setText("");num2.setText("");operator.setText("");
+            nextTextView = false;
+        }else if(operatorString.equalsIgnoreCase("/")){
+            finalAnswer = String.valueOf((inputText1 / inputText2));
+            num1.setText("");num2.setText("");operator.setText("");
             nextTextView = false;
         }
         answer.setText(finalAnswer);
